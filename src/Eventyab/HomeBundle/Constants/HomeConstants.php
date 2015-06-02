bfb85cfb4e4513faf0a2be32d34c2622a86624c1
Mohammad Ali <sma.jazayeri@gmail.com>
2015-06-01 23:41:51 -0700

    Added mine css

---
+++ src/Eventyab/HomeBundle/Constants/HomeConstants.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Eventyab\HomeBundle\Constants;
+
+class HomeConstants {
+	public static $team_name = ["Mohammad Soroush", "Mohammadreza Rahimi", 
+		"Mohammad Hadi Aghahassani", "Mohammad Ali Jazayeri", "Mohammad Moravvej"];
+	public static  $team_role = ["Co-founder & role", "Co-founder & role", "Co-founder & role", 
+		"Co-founder & role", "Co-founder & role"];
+	public static  $team_linkedin = ["https://www.linkedin.com/in/mohammadsoroush", "https://www.linkedin.com", "https://www.linkedin.com/in/hadiaghahassani", "https://www.linkedin.com/in/mojazayeri", "https://www.linkedin.com", "https://www.linkedin.com"];
+	public static  $team_bio = [
+		"Mohammad received his PhD and MEng in Petroleum Engineering from the University of Calgary. 
+	    As a highly motivated, energetic, and well-organized petroleum engineer, he has succeeded in 
+	    several leadership and mentorship professional activities. Mohammad is co-founder of Iranian 
+	    Graduate Student Association (IGSA). His voluntary activities have been started in IGSA as 
+        event manager and VP-communication and continued in many social events inside and outside the university",
+			
+		"Mohammadreza received his M.Sc. in Geomatics Engineering from the University of Calgary, for developing 
+        location recommendation systems for social networking services. Prior to that, he received a bachelor's 
+        degree in Software Engineering from Sharif University of Technology in Iran. Mohammadreza is currently 
+        the president of Iranian Graduate Students’ Association at the University of Calgary. Serving at this 
+        position and as a volunteer in other positions, he has developed a large set of leadership skills which 
+        he aims to commercializing them at Eventyab. He is currently a PhD student at the University of Calgary 
+        and a web developer at Eventyab.",
+			
+		"Hadi is originally from Iran. He moved to Calgary in 2011 to obtain his Master's degree in structural 
+        engineering from the University of Calgary. Now he is working as a structural engineer with a consulting 
+        firm in Calgary, where he applies his academic background in building industry. Hadi enjoys volunteering 
+        for non-profit and charity organizations and loves to make new friends. He enjoys volleyball, swimming and 
+        spending time with his lovely wife and awesome friends.",
+			
+		"Mohammad Ali received his MSc in Geomatics Engineering from the University of Calgary, where he belongs to. 
+		During his Masters, Mohammad has done many voluntary activities in a spread spectrum of event advertisement 
+		and event organizer to presidency of a governmental association. As he's a sociable person, he loves to do 
+		gathering with his friends in all social events. To simplify this process, he started developing a web 
+		application benefiting all people across the world. Having an optimistic attitude, he believes in success for 
+		the future of this application.",
+			
+		"Mohammad, so called Morooj or MHMH, loves spending time with friends and having fun activities with them, 
+        no matter it is a daily gathering over a coffee or a big celebration like Nowruz! Morooj met Mohammad Ali 
+        (Shahram), Mamrooz, and Soroush when studying his MSc at the University of Calgary, whilst already knew Hadi 
+        from memorable undergrad years at the Sharif University of Technology. MHMH have been doing volunteer works 
+        since he entered Sharif University and he has never gave up on that until now. Head of Research and Education, 
+        VP-Events, and VP-Treasury of different groups and organizations in Iran and in Canada are some of the examples 
+        of his leadership experience. Mohammad is currently pursuing his PhD at the University of Calgary and cooperates 
+        with Civil Engineering Graduate Society (CEGS) as the VP-Events."
+	];
+	
+	public static $team_image = ["img/team/ms.jpg", "img/team/mr.jpg", "img/team/ma.jpg", "img/team/mj.jpg", "img/team/mm.jpg"];
+}
+?>
